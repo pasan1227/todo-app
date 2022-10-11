8d2ade8a6934a9843d78bfd99919e3fa17a0f714
Pasan <pasanratnayake@gmail.com>
2022-10-11 15:08:44 +0530

    Task Component

---
+++ client/src/components/Tasks.js
@@ -0,0 +1,16 @@
+const Tasks = [
+  {
+    id: '1',
+    Name: 'Task 1',
+  },
+  {
+    id: '2',
+    Name: 'Task 2',
+  },
+  {
+    id: '3',
+    Name: 'Task 3',
+  },
+];
+
+export default Tasks
